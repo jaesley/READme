38df76a28677a7f4c8ae25cab00c59092d2fde4f
Jae Easley <jaesley@gmail.com>
2017-08-22 11:45:13 -0700

    Rearrange org structure to be more RESTful

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+  def show
+    if user_signed_in?
+      request = RestClient.get "http://www.goodreads.com/review/list/#{current_user.uid}?key=#{ENV['GOODREADS_API_KEY']}&v=2&sort=author&per_page=200&shelf=read"
+      @body = Hash.from_xml(request)
+      @authors = []
+      @body['GoodreadsResponse']['reviews']['review'].each do |review|
+        author = {name: review['book']['authors']['author']['name'], goodreads_id: review['book']['authors']['author']['id']}
+        @authors << author
+      end
+      @authors.uniq!
+    end
+  end
 end
