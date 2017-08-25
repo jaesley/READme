aa9b5c50f0ed890f2d60a16870ce82b9d1f6851c
Jae Easley <jaesley@gmail.com>
2017-08-25 13:44:43 -0700

    Refactor author creation calls

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,40 +1,7 @@
 class BooksController < ApplicationController
   def create
     if user_signed_in?
-      authors_request = RestClient.get "http://www.goodreads.com/review/list/#{current_user.uid}?key=#{ENV['GOODREADS_API_KEY']}&v=2&sort=author&per_page=200&shelf=read"
-      @body = Hash.from_xml(authors_request)
-      @authors = []
-      @body['GoodreadsResponse']['reviews']['review'].each do |review|
-        author = {name: review['book']['authors']['author']['name'], goodreads_id: review['book']['authors']['author']['id']}
-        @authors << author
-      end
-      @authors.uniq!.each do |author|
-        author = Author.find_or_create_by(author)
-        if !current_user.authors.include? author
-          current_user.authors << author
-        end
-        books_request = RestClient.get "http://www.goodreads.com/author/list/#{author.goodreads_id}?key=#{ENV['GOODREADS_API_KEY']}&v=2&per_page=200"
-        books_xml = Hash.from_xml(books_request)
-        books_xml['GoodreadsResponse']['author']['books']['book'].each do |book|
-          pub_date = nil
-          begin
-          if book['publication_year'] != nil || book['publication_month'] != nil || book['publication_day'] != nil
-            pub_date = "#{book['publication_year']}-#{book['publication_month']}-#{book['publication_day']}"
-              if Date.parse(pub_date) && Date.parse(pub_date) >= Date.today
-                title = book['title_without_series']
-                goodreads_id = book['id']
-
-                book['isbn'] ? isbn = book['isbn'] : isbn = book['isbn10']
-
-                book = {title: title, goodreads_id: goodreads_id, isbn: isbn, author_id: author.id, publication_date: pub_date}
-                author.books << Book.find_or_create_by(book)
-              end
-          end
-          rescue
-            next
-          end
-        end
-      end
+      current_user.generate_records
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,69 @@ class User < ApplicationRecord
    def email_changed?
      false
    end
+
+   def generate_records
+     generate_authors
+     generate_books
+   end
+
+   def generate_authors
+     authors_request = RestClient.get "http://www.goodreads.com/review/list/#{current_user.uid}?key=#{ENV['GOODREADS_API_KEY']}&v=2&sort=author&per_page=200&shelf=read"
+     authors = generate_author_hashes(Hash.from_xml(authors_request))
+     authors.each do |author|
+       generate_author(author)
+     end
+   end
+
+   def generate_author(author)
+     author = Author.where(goodreads_id: author[:goodreads_id]).first_or_create.update_attributes(name: author[:name])
+     generate_follow(author)
+   end
+
+   def generate_follow(author)
+     if !current_user.authors.include? author
+       current_user.authors << author
+     end
+   end
+
+   def generate_author_hashes(body)
+     authors = []
+     body['GoodreadsResponse']['reviews']['review'].each do |review|
+       author = generate_author_hash(review)
+       authors << author
+     end
+     authors.uniq
+   end
+
+   def generate_author_hash(review)
+     name = review['book']['authors']['author']['name']
+     goodreads_id = review['book']['authors']['author']['id']
+     {name: name, goodreads_id: goodreads_id}
+   end
+
+   def generate_books
+
+       books_request = RestClient.get "http://www.goodreads.com/author/list/#{author.goodreads_id}?key=#{ENV['GOODREADS_API_KEY']}&v=2&per_page=200"
+       books_xml = Hash.from_xml(books_request)
+       books_xml['GoodreadsResponse']['author']['books']['book'].each do |book|
+         pub_date = nil
+         begin
+         if book['publication_year'] != nil || book['publication_month'] != nil || book['publication_day'] != nil
+           pub_date = "#{book['publication_year']}-#{book['publication_month']}-#{book['publication_day']}"
+             if Date.parse(pub_date) && Date.parse(pub_date) >= Date.today
+               title = book['title_without_series']
+               goodreads_id = book['id']
+
+               book['isbn'] ? isbn = book['isbn'] : isbn = book['isbn10']
+
+               book = {title: title, goodreads_id: goodreads_id, isbn: isbn, author_id: author.id, publication_date: pub_date}
+               author.books << Book.find_or_create_by(book)
+             end
+         end
+         rescue
+           next
+         end
+       end
+     end
+   end
 end
