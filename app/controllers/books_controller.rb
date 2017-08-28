7c994d067a2bae01099b8f0e9978e7181b9c7a02
Jae Easley <jaesley@gmail.com>
2017-08-28 10:33:29 -0700

    Add hero image code

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,9 @@
 class BooksController < ApplicationController
   include ApplicationHelper
-  
+
   def create
     if user_signed_in?
-      generate_records(current_user)
+      # generate_records(current_user)
     end
   end
 end
