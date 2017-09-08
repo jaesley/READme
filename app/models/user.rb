ad7ce3038b494cf10b0092cb2dcca3564c6817a4
Jae Easley <jaesley@gmail.com>
2017-09-08 16:41:34 -0700

    Refactor shortcutting to speed response time

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,22 +66,21 @@ class User < ApplicationRecord
   def generate_authors_all_pages
     page_number = 1
     total_reviews = 0
-    request = Typhoeus::Request.new("http://www.goodreads.com/review/list/#{uid}?key=#{ENV['GOODREADS_API_KEY']}&sort=author&page=#{page_number}&per_page=200&shelf=read")
+    request = Typhoeus::Request.new("http://www.goodreads.com/review/list/#{uid}?key=#{ENV['GOODREADS_API_KEY']}&sort=author&page=#{page_number}&per_page=1&shelf=read")
     request.on_complete do |response|
       response = Hash.from_xml(response.body)
-      generate_authors(response)
       total_reviews = response['GoodreadsResponse']['books']['total'].to_i
     end
     request.run
 
     if total_reviews != reviews_count
       update_attributes(reviews_count: total_reviews)
-      page_count = total_reviews / 200
+      page_count = (total_reviews / 200) + 1
 
-      hydra = Typhoeus::Hydra.hydra
+      hydra = Typhoeus::Hydra.new(max_concurrency: 200)
 
       page_count.times do |x|
-        page_number = x + 2
+        page_number = x + 1
         request = Typhoeus::Request.new("http://www.goodreads.com/review/list/#{uid}?key=#{ENV['GOODREADS_API_KEY']}&sort=author&page=#{page_number}&per_page=200&shelf=read")
         request.on_complete do |response|
           response = Hash.from_xml(response.body)
