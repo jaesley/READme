87aaa966069f9f2a694bff529ae54f36137daaa9
Jae Easley <jaesley@gmail.com>
2017-09-07 22:36:10 -0700

    Fix validation error

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,5 @@ class Book < ApplicationRecord
   has_many :followers, through: :follows, source: :user
 
   validates :title, :goodreads_id, :publication_date, presence: true
+  validates :goodreads_id, uniqueness: true
 end
