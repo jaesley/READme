0926a5628b1f1eefc1199034685a21fd8ead6354
Jae Easley <jaesley@gmail.com>
2017-09-07 21:18:17 -0700

    Fix typo

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
     if user_signed_in?
       current_user.generate_authors_all_pages
       current_user.authors.each do |author|
-        user.generate_books_page(author)
+        current_user.generate_books_page(author)
       end
     end
   end
