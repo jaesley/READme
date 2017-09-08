baa0ad1ff23596a081d0364e0989d8fe91e5afaa
Jae Easley <jaesley@gmail.com>
2017-09-07 19:47:04 -0700

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -79,18 +79,17 @@ RSpec.describe User, type: :model do
     end
 
     context '#generate_authors_all_pages' do
-      let(:data) { user.generate_authors_all_pages }
-      let(:page1) { user.generate_authors_single_page(1) }
-      let(:page2) { user.generate_authors_single_page(2) }
-      let(:page3) { user.generate_authors_single_page(3) }
-      let(:page4) { user.generate_authors_single_page(4) }
-      let(:page5) { user.generate_authors_single_page(5) }
-      let(:page6) { user.generate_authors_single_page(6) }
-
       it 'aggregates data from each page of a read shelf' do
-        pages = [page1, page2, page3, page4, page5, page6]
-        total = pages.reduce(0) { |sum, page| sum + page[:author_hashes].length }
-        expect(data.length).to eq(total)
+        user.generate_authors_single_page(1)
+        user.generate_authors_single_page(2)
+        user.generate_authors_single_page(4)
+        user.generate_authors_single_page(5)
+        user.generate_authors_single_page(6)
+        total = Author.count
+        page = RestClient.get "http://www.goodreads.com/review/list/#{user.uid}?key=#{ENV['GOODREADS_API_KEY']}&sort=author&per_page=5&shelf=read"
+        page = Hash.from_xml(page.to_s)
+        
+        expect{user.generate_authors_all_pages}.to change(Author, :count).from(0).to(total)
       end
     end
   end
