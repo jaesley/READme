004be546f9719262845abfab61c16588ed2b06a7
Jae Easley <jaesley@gmail.com>
2017-09-11 12:06:47 -0700

    Update follow.rb

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,7 @@
 class Follow < ApplicationRecord
   belongs_to :author
   belongs_to :user
+  
+  validates :author, presence: true, uniqueness: {:scope => :user, 
+    message: "User is already following author." }
 end
