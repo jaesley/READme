761e7fb08dcd8fda47d36befe69666e7ebda9f95
Jae Easley <jaesley@gmail.com>
2017-11-15 19:00:09 -0800

    Restyle items with cards via Bootstrap

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
   include ApplicationHelper
 
   def create
+    current_user = User.first
     if user_signed_in?
       current_user.generate_authors_all_pages
       current_user.authors.each do |author|
